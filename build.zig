7e167a4399179222b5ab8297689da70c3a19213b
Felix (xq) Queißner <git@mq32.de>
2020-05-15 01:29:49 +0200

    Refactors BearSSL code into bearssl.zig.

--- build.zig
+++ build.zig
@@ -2,9 +2,13 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
     const mode = b.standardReleaseOptions();
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{
+        .default_target = try std.zig.CrossTarget.parse(.{
+            .arch_os_abi = "native-native-gnu.2.25..2.25", // we need at least glibc 2.25 for getentropy
+        }),
+    });
 
-    const gurl = b.addExecutable("gget", "src/main.zig");
+    const gurl = b.addExecutable("gurl", "src/main.zig");
     gurl.setBuildMode(mode);
     gurl.setTarget(target);
     gurl.linkLibC();
